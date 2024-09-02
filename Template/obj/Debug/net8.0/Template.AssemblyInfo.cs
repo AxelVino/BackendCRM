c11048b374987d396c356a237c5c243f68cfdacd
AxelVino <UnajAxel@gmail.com>
2024-09-02 11:21:55 -0300

    Template inicial

--- Template/obj/Debug/net8.0/Template.AssemblyInfo.cs
+++ Template/obj/Debug/net8.0/Template.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Template")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c69f05569a7262407b12c84042b510132001bd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Template")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Template")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
